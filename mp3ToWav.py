fb4d3284159f070de3230f521034a7b110a2c51b
KLB <k.l.blaedel@gmail.com>
2015-11-17 13:26:11 +0900

    Forgot mp3towav

--- mp3ToWav.py
+++ mp3ToWav.py
@@ -1,25 +1,20 @@
-import sys
 import CloudConvert
 import os
 from os import path
-import apiKey
-
-
-input_file = 'audio.mp3'
-output_file = 'out.mp3'
-api_key = ''
-
-API_KEY = 'cloudconvertAPI.key'
+import keys.apiKey as apiKey
 
 dir_path = path.dirname(path.realpath(__file__))
 
-process = CloudConvert.ConversionProcess(apiKey.getKey(API_KEY))
+apikey = apiKey.getKey('keys/cc.key')
+
+process = CloudConvert.ConversionProcess(apikey)
 
 # This should autodetect file extension. if not, you can
 # always set process.fromformat and .toformat to the correct
 # values
 
-process.init(input_file, output_file)
+# process.init(path.join(dir_path, "audio.mp3"), "out.wav")
+process.init("audio.mp3", "out.wav")
 
 # This step uploads the file and starts the conversion.
 # If you pass `wait=True` the request will block until the
